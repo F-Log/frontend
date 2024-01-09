3650ed31038a738431ec7de7e4b2bd41cfd7e0e5
LeeFavor <ljh001011@gmail.com>
2024-01-09 14:30:26 +0900

    feat/#1

---
+++ src/app/Log.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Log() {
+  return <div>Log Page</div>;
+}
+
+export default Log;
---
+++ src/app/OCR.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function OCR() {
+  return <div>OCR Page</div>;
+}
+
+export default OCR;
