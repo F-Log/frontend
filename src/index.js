005fae94a9e0ac37dd7a7ccbaded02077365509e
LeeFavor <ljh001011@gmail.com>
2024-01-12 16:56:19 +0900

    fix: 잘못된 파일 feat

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import MyApp from './myblog/blog';
+import App from './app/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MyApp />
+    <App />
   </React.StrictMode>
 );
 
